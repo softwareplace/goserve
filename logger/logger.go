28fe5d67135ac1e82f341c6994e20d3814777609
Elias Meireles <eliasmeirelesf@gmail.com>
2025-03-22 15:21:16 -0300

    Refactor logger initialization and environment utilities
    Refactored the logger for simplified configuration with sensible defaults, introducing the `DefaultLumberjackLogger` and removing redundant setup code. Added utility methods `UserHomePathFix`, `GetEnvOrDefault`, and `GetBoolEnvOrDefault` for handling environment variables and paths more efficiently. Enhanced caller reporting and log rotation handling through streamlined logic.

--- logger/logger.go
+++ logger/logger.go
@@ -11,74 +11,48 @@ import (
 )
 
 var (
-	logDateFormat = "2006-01-02"      // logDateFormat defines the date format (YYYY-MM-DD) used for naming log files and tracking the current date.
-	logFile       *os.File            // Global variable to keep the file open
-	logDirPath    string              // Global variable to store the log directory path
-	logFilePath   string              // Global variable to store the full log file path
-	currentDate   string              // Global variable to track the current date
-	appName       string         = "" // appName holds the name of the application for logging and identification purposes.
+	logDateFormat         = "2006-01-02"     // logDateFormat defines the date format (YYYY-MM-DD) used for naming log files and tracking the current date.
+	logFileNameDateFormat = "2006-01-02"     // logDateFormat defines the date format (YYYY-MM-DD) used for naming log files and tracking the current date.
+	logFile               *os.File           // Global variable to keep the file open
+	logDirPath            string             // Global variable to store the log directory path
+	logFilePath           string             // Global variable to store the full log file path
+	currentDate           string             // Global variable to track the current date
+	appName               = ""               // appName holds the name of the application for logging and identification purposes.
+	LogReportCaller       = false            // LogReportCaller determines whether the logger includes caller information (file and line number) in log messages.
+	LumberjackLogger      *lumberjack.Logger // LumberjackLogger is initialized as the default rotating logger for log file management via DefaultLumberjackLogger.
 )
 
 func init() {
-	logDirPath = "./.log"
-	if envLogPath, exists := os.LookupEnv("LOG_DIR"); exists && envLogPath != "" {
-		logDirPath = envLogPath
-	}
-
-	if envAppName, exists := os.LookupEnv("LOG_APP_NAME"); exists && envAppName != "" {
-		appName = envAppName
-	}
-
+	logFileNameDateFormat = utils.GetEnvOrDefault("LOG_FILE_NAME_DATE_FORMAT", "2006-01-02")
+	logDirPath = utils.GetEnvOrDefault("LOG_DIR", "./.log/")
 	logDirPath = utils.UserHomePathFix(logDirPath)
+	appName = utils.GetEnvOrDefault("LOG_APP_NAME", "")
+	LogReportCaller = utils.GetBoolEnvOrDefault("LOG_REPORT_CALLER", false)
+	LumberjackLogger = DefaultLumberjackLogger()
 }
 
-type Config struct {
-	logDirPath string // Global variable to store the log directory path
-	// LogDateFormat defines the date format (e.g., "2006-01-02") for naming log files.
-	// Default value is "2006-01-02".
-	LogDateFormat string
-
-	// LumberjackLogger represents the configuration for the log rotation mechanism
-	// using lumberjack.Logger.
-	// Default DefaultLumberjackLogger()
-	LumberjackLogger *lumberjack.Logger
-}
-
-func LumberjackLoggerSetup(config Config) {
-	if config.LogDateFormat != "" {
-		logDateFormat = config.LogDateFormat
-	}
-	if config.LumberjackLogger != nil {
-		LogSetup(logDirPath, config.LumberjackLogger)
-		return
-	}
-
-	if config.logDirPath != "" {
-		logDirPath = utils.UserHomePathFix(config.logDirPath)
-	}
-
-	LogSetup(logDirPath, DefaultLumberjackLogger())
-}
-
-func LogSetup(dirPath string, logger *lumberjack.Logger) {
-	logDirPath = dirPath
-	createLogFile(logger)
-	go monitorLogFile(logger)
+// LogSetup initializes the logging system by creating a log file and starting a monitoring routine for file rotation.
+// Default:
+//   - LumberjackLogger used as default. You can also change it
+func LogSetup() {
+	createLogFile()
+	go monitorLogFile()
 }
 
 // createLogFile creates or reopens the log file
-func createLogFile(logger *lumberjack.Logger) {
+func createLogFile() {
 	if err := os.MkdirAll(logDirPath, 0755); err != nil {
 		log.Fatalf("Failed to create log directory: %v", err)
 	}
 
 	// Generate the log file name with the current date
-	currentDate = time.Now().Format(logDateFormat) // yyyy-MM-dd format
+	currentDate = time.Now().Format(logFileNameDateFormat)
 	logFileName := currentDate + ".log"
 
 	if appName != "" {
 		logFileName = appName + "-" + logFileName
 	}
+
 	logFilePath = filepath.Join(logDirPath, logFileName)
 
 	// Open or create the log file
@@ -88,14 +62,24 @@ func createLogFile(logger *lumberjack.Logger) {
 		log.Fatalf("Failed to open log file: %v", err)
 	}
 	// Create a multi-writer that writes to both the terminal and the file
-	multiWriter := io.MultiWriter(os.Stdout, DefaultLumberjackLogger())
+	LumberjackLogger.Filename = logFilePath
+
+	multiWriter := io.MultiWriter(os.Stdout, LumberjackLogger)
 	// Set the output of the logger to the multi-writer
 	log.SetOutput(multiWriter)
+	log.SetReportCaller(LogReportCaller)
 }
 
+// DefaultLumberjackLogger returns a configured Lumberjack logger instance.
+// The logger is used for managing file-based log rotation.
+// Configuration:
+//   - Filename: Path to the log file for writing logs.
+//   - MaxSize: Maximum size of a log file in MB before rotation occurs.
+//   - MaxBackups: Maximum number of old log files to retain.
+//   - MaxAge: Maximum number of days old log files are retained.
+//   - Compress: Specifies whether to compress old log files.
 func DefaultLumberjackLogger() *lumberjack.Logger {
 	return &lumberjack.Logger{
-		Filename:   logFilePath,
 		MaxSize:    100,  // Max file size in MB
 		MaxBackups: 3,    // Max number of old log files to retain
 		MaxAge:     28,   // Max number of days to retain old log files
@@ -104,26 +88,26 @@ func DefaultLumberjackLogger() *lumberjack.Logger {
 }
 
 // monitorLogFile periodically checks if the log file exists and recreates it if necessary
-func monitorLogFile(logger *lumberjack.Logger) {
+func monitorLogFile() {
 	for {
 		time.Sleep(1 * time.Second) // Check every second
 
 		// Check if the current date has changed
-		newDate := time.Now().Format(logDateFormat)
+		newDate := time.Now().Format(logFileNameDateFormat)
 		if newDate != currentDate {
-			rotateLogFile(newDate, logger)
+			rotateLogFile(newDate)
 		}
 
 		// Check if the log file still exists
 		if _, err := os.Stat(logFilePath); os.IsNotExist(err) {
 			log.Println("Log file was deleted. Recreating...")
-			createLogFile(logger) // Recreate the log file
+			createLogFile() // Recreate the log file
 		}
 	}
 }
 
 // rotateLogFile closes the current log file and creates a new one with the updated date
-func rotateLogFile(newDate string, logger *lumberjack.Logger) {
+func rotateLogFile(newDate string) {
 	// Close the current log file
 	if logFile != nil {
 		err := logFile.Close()
@@ -136,5 +120,5 @@ func rotateLogFile(newDate string, logger *lumberjack.Logger) {
 	currentDate = newDate
 
 	// Create a new log file with the updated date
-	createLogFile(logger)
+	createLogFile()
 }
---
+++ utils/user_path_fix.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+	"strings"
+)
+
+// UserHomePathFix resolves a path that starts with '~' to the user's home directory.
+// If the home directory cannot be determined, it logs an error and exits the application.
+// Parameters:
+//   - path: The file path string, potentially starting with '~'.
+//
+// Returns:
+//   - The resolved file path with '~' replaced by the user's home directory, or the original path if no substitution is needed.
+func UserHomePathFix(path string) string {
+	if strings.HasPrefix(path, "~") {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Printf("Error: Unable to resolve user home directory for path: %v\n", err)
+			os.Exit(1)
+		}
+		path = strings.Replace(path, "~", homeDir, 1)
+	}
+	return path
+}
