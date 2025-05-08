73ce6001681fa456cc02d1e3332f32e8a55ac9a2
Elias Meireles <eliasmeirelesf@gmail.com>
2025-05-08 12:06:47 -0300

    Refactor flag initialization logic to improve testability
    Introduced `InitFlagsWithSet` to allow passing custom flag sets and arguments, simplifying testing and reducing reliance on global state. Updated test cases accordingly to leverage the new function and improve maintainability.

--- cmd/goserve-generator/config/flags.go
+++ cmd/goserve-generator/config/flags.go
@@ -49,21 +49,32 @@ func argsValidation() {
 }
 
 func InitFlags() {
-	flag.StringVar(&ProjectName, "n", "", "Project name")
-	flag.StringVar(&Username, "u", "", "GitHub username")
-	flag.StringVar(&ReplaceCurrent, "r", "false", "Replace current directory/files with generated files")
-	flag.StringVar(&GiInit, "gi", "true", "(optional): Git project initialization")
-	flag.StringVar(&CodeGenConfigFile, "cgf", "", "(optional): template of the codegen config file")
-	flag.StringVar(&GoServerVersion, "gsv", "", "(optional): use a specific version of goserver")
+	_ = InitFlagsWithSet(flag.CommandLine, os.Args[1:])
+}
 
-	flag.Usage = flagUsage
+func InitFlagsWithSet(fs *flag.FlagSet, args []string) error {
+	fs.StringVar(&ProjectName, "n", "", "Project name")
+	fs.StringVar(&Username, "u", "", "GitHub username")
+	fs.StringVar(&ReplaceCurrent, "r", "false", "Replace current directory/files with generated files")
+	fs.StringVar(&GiInit, "gi", "true", "(optional): Git project initialization")
+	fs.StringVar(&CodeGenConfigFile, "cgf", "", "(optional): template of the codegen config file")
+	fs.StringVar(&GoServerVersion, "gsv", "", "(optional): use a specific version of goserver")
+
+	fs.Usage = flagUsage
+
+	err := fs.Parse(args)
+	if err != nil {
+		return err
+	}
 
 	argsValidation()
+
+	return nil
 }
 
 func flagUsage() {
 	_, _ = fmt.Fprintf(os.Stderr, "\nUsage: goserve-generator [options]\n")
 	flag.PrintDefaults()
 	fmt.Printf("  version\n\tCheck the current version of goserve-generator")
-	fmt.Printf("  update\n\tUpdate goserve-generator to the latest version")
+	fmt.Printf("  update\n\tUpdate goserve-generator to the latest version\n")
 }
--- cmd/goserve-generator/config/flags_test.go
+++ cmd/goserve-generator/config/flags_test.go
@@ -2,212 +2,54 @@ package config
 
 import (
 	"flag"
-	"github.com/softwareplace/goserve/cmd/goserve-generator/version"
-	"log"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
-// MockVersionChecker is a mock for version package
-type MockVersionChecker struct {
-	mock.Mock
-}
+func TestInitFlagsWithSet(t *testing.T) {
+	t.Cleanup(func() {
+		// Reset global vars after each test
+		ProjectName = ""
+		Username = ""
+		ReplaceCurrent = ""
+		GiInit = ""
+		CodeGenConfigFile = ""
+		GoServerVersion = ""
+	})
 
-func (m *MockVersionChecker) CheckCurrentVersion() {
-	m.Called()
-}
-
-func (m *MockVersionChecker) Update() {
-	m.Called()
-}
-
-// Test variables to restore original implementations
-var (
-	originalArgs          = os.Args
-	originalExit          = osExit
-	originalVersionCheck  = version.CheckCurrentVersion
-	originalVersionUpdate = version.Update
-)
-
-func restoreOriginals() {
-	os.Args = originalArgs
-	osExit = originalExit
-	checkVersion = originalVersionCheck
-	update = originalVersionUpdate
-}
-
-func TestInitFlags(t *testing.T) {
-	t.Cleanup(restoreOriginals)
-	t.Cleanup(func() { flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError) })
-
-	test := struct {
-		name        string
-		args        []string
-		expectPanic bool
-		expected    struct {
-			ProjectName    string
-			Username       string
-			ReplaceCurrent string
-			GiInit         string
-		}
-	}{
-		name: "all flags",
-		args: []string{"cmd", "-n", "myproject", "-u", "myuser"},
-		expected: struct {
-			ProjectName    string
-			Username       string
-			ReplaceCurrent string
-			GiInit         string
-		}{
-			ProjectName:    "myproject",
-			Username:       "myuser",
-			ReplaceCurrent: "false",
-			GiInit:         "true",
-		},
+	args := []string{
+		"-n", "myproject",
+		"-u", "myuser",
+		"-r", "true",
+		"-gi", "false",
+		"-cgf", "template.yml",
+		"-gsv", "v1.2.3",
 	}
 
-	t.Run(test.name, func(t *testing.T) {
+	fs := flag.NewFlagSet("test", flag.ContinueOnError)
+	err := InitFlagsWithSet(fs, args)
+	assert.NoError(t, err)
 
-		os.Args = test.args
-
-		InitFlags()
-
-		assert.Equal(t, test.expected.ProjectName, ProjectName)
-		assert.Equal(t, test.expected.Username, Username)
-		assert.Equal(t, test.expected.ReplaceCurrent, ReplaceCurrent)
-		assert.Equal(t, test.expected.GiInit, GiInit)
-		//assert.Equal(t, flag.Usage, flagUsage)
-	})
+	assert.Equal(t, "myproject", ProjectName)
+	assert.Equal(t, "myuser", Username)
+	assert.Equal(t, "true", ReplaceCurrent)
+	assert.Equal(t, "false", GiInit)
+	assert.Equal(t, "template.yml", CodeGenConfigFile)
+	assert.Equal(t, "v1.2.3", GoServerVersion)
 }
 
-func TestInitFlags2(t *testing.T) {
-	t.Cleanup(restoreOriginals)
-	t.Cleanup(func() { flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError) })
-
-	test := struct {
-		name        string
-		args        []string
-		expectPanic bool
-		expected    struct {
-			ProjectName    string
-			Username       string
-			ReplaceCurrent string
-			GiInit         string
-		}
-	}{
-		name: "all flags",
-		args: []string{"cmd", "-n", "myproject", "-u", "myuser", "-r", "true", "-gi", "false"},
-		expected: struct {
-			ProjectName    string
-			Username       string
-			ReplaceCurrent string
-			GiInit         string
-		}{
-			ProjectName:    "myproject",
-			Username:       "myuser",
-			ReplaceCurrent: "true",
-			GiInit:         "false",
-		},
-	}
-
-	t.Run(test.name, func(t *testing.T) {
-
-		os.Args = test.args
-
-		InitFlags()
-
-		assert.Equal(t, test.expected.ProjectName, ProjectName)
-		assert.Equal(t, test.expected.Username, Username)
-		assert.Equal(t, test.expected.ReplaceCurrent, ReplaceCurrent)
-		assert.Equal(t, test.expected.GiInit, GiInit)
-	})
-}
-
-func TestArgsValidation(t *testing.T) {
-	t.Cleanup(restoreOriginals)
-	t.Cleanup(func() { flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError) })
-
-	// Mock os.Exit
-	exitCalled := false
-	exitCode := 0
+func TestInitFlagsWithSet_MissingRequiredFlags(t *testing.T) {
+	called := false
+	oldExit := osExit
 	osExit = func(code int) {
-		exitCalled = true
-		exitCode = code
+		called = true
 	}
+	defer func() { osExit = oldExit }()
 
-	// Mock version functions
-	mockVersion := new(MockVersionChecker)
-	checkVersion = mockVersion.CheckCurrentVersion
-	update = mockVersion.Update
+	fs := flag.NewFlagSet("test", flag.ContinueOnError)
+	args := []string{}
+	_ = InitFlagsWithSet(fs, args)
 
-	tests := []struct {
-		name           string
-		args           []string
-		setupFlags     func()
-		expectExit     bool
-		expectExitCode int
-		expectVersion  bool
-		expectUpdate   bool
-	}{
-		{
-			name:           "version command",
-			args:           []string{"cmd", "version"},
-			expectExit:     true,
-			expectExitCode: 0,
-			expectVersion:  true,
-		},
-		{
-			name:           "update command",
-			args:           []string{"cmd", "update"},
-			expectExit:     true,
-			expectExitCode: 0,
-			expectUpdate:   true,
-		},
-		{
-			name: "missing required flags",
-			args: []string{"cmd"},
-			setupFlags: func() {
-				ProjectName = ""
-				Username = ""
-			},
-			expectExit:     true,
-			expectExitCode: 1,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			exitCalled = false
-			os.Args = tt.args
-
-			if tt.setupFlags != nil {
-				tt.setupFlags()
-			}
-
-			if tt.expectVersion {
-				mockVersion.On("CheckCurrentVersion").Once()
-			}
-			if tt.expectUpdate {
-				mockVersion.On("Update").Once()
-			}
-
-			argsValidation()
-
-			assert.Equal(t, tt.expectExit, exitCalled)
-			if tt.expectExit {
-				assert.Equal(t, tt.expectExitCode, exitCode)
-			}
-
-			mockVersion.AssertExpectations(t)
-		})
-	}
-}
-
-func TestInit(t *testing.T) {
-	// Verify log flags are set correctly
-	assert.Equal(t, 0, log.Flags())
-
+	assert.True(t, called, "Expected osExit to be called due to missing required flags")
 }
